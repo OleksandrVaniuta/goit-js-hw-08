ed7070c0bcf8c63e499fff80cc0da95a960166a8
OleksandrVaniuta <vaniutasa@gmail.com>
2023-01-31 09:56:10 +0200

    виправили помилки в консолі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,4 +10,20 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
 const parsedTime = JSON.parse(savedTime);
-player.setCurrentTime(parsedTime.seconds);
+
+player
+  .setCurrentTime(parsedTime.seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
