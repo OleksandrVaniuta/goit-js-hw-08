88295ef8e98f8a12bc6a919ce73c330ee4145d54
OleksandrVaniuta <vaniutasa@gmail.com>
2023-02-01 13:04:54 +0200

    виправив задачу номер 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,12 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const onPlay = function (time) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
-};
+
 player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
 setCurrentTime();
 function setCurrentTime() {
